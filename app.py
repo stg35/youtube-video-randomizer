659c9855b421ff522f79ceba500eb0fd4c4d98cb
Alex <alex30052003@icloud.com>
2020-06-11 11:06:04 +0300

    no message

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask
+import os
 
-app = Flask(__name__)
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
 
+scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+def main():
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = "CLIENT_SECRET_FILE.json"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    # Get credentials and create an API client
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes)
+    credentials = flow.run_console()
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
+
+    request = youtube.channels().list(
+        part="snippet,contentDetails,statistics",
+        id="UC_x5XG1OV2P6uZZ5FSM9Ttw"
+    )
+    response = request.execute()
+
+    print(response)
+
+if __name__ == "__main__":
+    main()
