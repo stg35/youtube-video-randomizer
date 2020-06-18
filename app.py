74bea44427af8ca7ebb46f0ebadaeaa96dab9898
Alex <alex30052003@icloud.com>
2020-06-18 23:14:35 +0300

    all data

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ def main():
             videoID = item['contentDetails']['videoId']
             request_video = requests.get('https://www.googleapis.com/youtube/v3/videos?id={0}&key={1}&part=snippet,contentDetails,statistics'.format(videoID, API_KEY))
             response_video = request_video.json()
-            print(videoID)
             if response_video['items']:
                 publishedAt = response_video['items'][0]['snippet']['publishedAt']
                 title = response_video['items'][0]['snippet']['title']
@@ -37,7 +36,15 @@ def main():
                             i-=1
                         s = int(duration[i+1:point])
                 duration = h*3600 + m*60 + s
-                videos.append({'videoID': videoID, 'publishedAt': publishedAt, 'title': title, 'duration': duration})
+                viewCount = response_video['items'][0]['statistics']['viewCount']
+                likeCount = response_video['items'][0]['statistics']['likeCount']
+                dislikeCount = response_video['items'][0]['statistics']['dislikeCount']
+                commentCount = response_video['items'][0]['statistics']['commentCount']
+                tags = response_video['items'][0]['snippet']['tags']
+                videos.append({'videoID': videoID, 'publishedAt': publishedAt, 'title': title,
+                               'duration': duration, 'viewCount': viewCount, 'likeCount': likeCount,
+                               'dislikeCount': dislikeCount, 'commentCount': commentCount, 'tags': tags,
+                               'playlistID': ID})
 
     print(videos)
 
